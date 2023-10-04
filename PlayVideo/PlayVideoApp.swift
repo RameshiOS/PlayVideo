b439ec2154627447211f9ed3022a543c5dd3d5a7
rameshios <ramesh.guddala@gmail.com>
2023-10-04 14:57:28 +0530

    Initial Commit

---
+++ PlayVideo/PlayVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayVideoApp.swift
+//  PlayVideo
+//
+//  Created by Ramesh Guddala on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
